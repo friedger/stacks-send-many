1c149ce740eb24b12b29479128397062bff8ff8c
Ömür Çataltepe <cataltepe@outlook.com.tr>
2021-04-28 11:30:47 +0300

    Update Profile and SendManyInput

--- src/components/SendManyInput.js
+++ src/components/SendManyInput.js
@@ -14,25 +14,32 @@ export function SendManyInput({ index, row, updateModel }) {
   };
 
   return (
-    <div key={index}>
-      <input
-        ref={to}
-        value={row.to}
-        onChange={e => updateModel(getRow())}
-        placeholder="recipient"
-      ></input>
-      <input
-        ref={stxValue}
-        value={row.stx}
-        onChange={e => updateModel(getRow())}
-        placeholder="amount"
-      ></input>
-      <input
-        ref={memo}
-        value={row.memo}
-        onChange={e => updateModel(getRow())}
-        placeholder="memo"
-      ></input>
-    </div>
+    <div key={index}> 
+      <div  className="row">
+        <div className="col-md-5 col-xs-5 col-lg-5">
+          <div className="form-label-group">
+            <input ref={to} value={row.to} onChange={e => updateModel(getRow())} placeholder="Wallet Address #1" type="text" className="form-control" required="" autofocus=""/>
+            <label for="inputEmail">Wallet Address #1</label>
+          </div>
+        </div>
+        <div className="col-md-3 col-xs-3 col-lg-3">
+          <div className="form-label-group">
+            <input ref={memo} value={row.memo} onChange={e => updateModel(getRow())} placeholder="0" type="text" className="form-control" required="" autofocus=""/>
+            <label for="inputEmail">Memo Tag #1</label>
+          </div>
+        </div>
+        <div className="col-md-3 col-xs-3 col-lg-3">
+          <div className="form-label-group">
+            <input ref={stxValue} value={row.stx} onChange={e => updateModel(getRow())} placeholder="0" type="text" maxlength="10" className="form-control" required="" autofocus=""/>
+            <label for="inputEmail">Amount #1</label>
+          </div>
+        </div> 
+        <div className="col-md-1 col-xs-1 col-lg-1">
+          <div className="form-label-group">
+          <input type="button" class="removeField btn btn-danger btn-sm" value="X"/>
+          </div>
+        </div>
+      </div> 
+      </div> 
   );
 }
