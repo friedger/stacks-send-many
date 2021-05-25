21ba3ae94ba8b2adfe4599790ad4194625c5731a
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-05-25 07:06:16 -0700

    progress bars are cool

--- src/components/CityCoinRegister.js
+++ src/components/CityCoinRegister.js
@@ -1,4 +1,9 @@
 export function CityCoinRegister() {
+  const styles = {
+    width: '10%',
+  };
+
+  
 
   return (
     <>
@@ -6,9 +11,11 @@ export function CityCoinRegister() {
       <p>Before mining can begin, at least 20 miners must register with the contract to signal activation.</p>
       <ul>
         <li>Miners Registered: 2</li>
-        <li>Treshold: 20 Miners</li>
-        <li>10% complete</li>
+        <li>Threshold: 20 Miners</li>
       </ul>
+      <div className="progress mb-3">
+        <div className="progress-bar" role="progressbar" style={styles} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">10%</div>
+      </div>
       <p>Countdown goes here once active</p>
       <button className="btn btn-block btn-primary" type="button">
         Register
