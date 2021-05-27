d1a395bba1089fd6f9af01c47dd4e0734926c2e1
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-05-27 07:05:28 -0700

    adjust styles and update landing copy

--- src/components/Network.js
+++ src/components/Network.js
@@ -3,7 +3,7 @@ import { STACK_API_URL, testnet, mainnet } from '../lib/constants';
 export function Network() {
   return (
     <div
-      className={`rounded border-secondary d-flex justify-content-around my-1 ${
+      className={`rounded border-secondary d-flex justify-content-around my-1 px-2 ${
         mainnet ? 'bg-primary' : 'bg-secondary'
       }`}
       title={STACK_API_URL}
