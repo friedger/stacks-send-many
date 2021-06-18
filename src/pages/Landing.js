a231952146f14d70a25e13c7ceb17ecbd745c9ac
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-06-18 15:58:17 -0700

    update landing page layout and sizing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -9,50 +9,49 @@ export default function Landing(props) {
   return (
     <div className="Landing">
       <div className="jumbotron jumbotron-fluid pt-3 mb-0">
-        <div className="container">
-          <div className="panel-landing text-center mt-3">
-            <div className="card mt-4 border-info">
-              <div className="card-header">
-                <h5 className="card-title">CityCoins on Stacks</h5>
-              </div>
-              <div className="card-body">
-                <p className="card-text mb-3 text-left">
-                  With CityCoins, you can earn Bitcoin (BTC) and Stacks (STX) yield for yourself
-                  while the city accumulates crypto in a reserved treasury wallet as a result. Each
-                  city has their own coin, starting with Miami and MiamiCoin ($MIA).
-                </p>
-                <p className="card-text mb-3 text-left">
-                  <span className="fw-bold">Registration: </span>
-                  CityCoins only exist through mining, which requires 20 independent wallets to
-                  signal activation once the contract is deployed. No ICO, no pre-mine.
-                  <br />
-                  <br />
-                  <span className="fw-bold">Mining: </span>
-                  Anyone can mine CityCoins by forwarding STX into a CityCoins smart contract on the
-                  Stacks blockchain. 30% of the STX that miners forward is sent directly to a
-                  reserved wallet for the city.
-                  <br />
-                  <br />
-                  <span className="fw-bold">Stacking: </span>
-                  The remaining 70% of the STX that miners forward is distributed to CityCoin
-                  holders who stack their tokens. Stacking CityCoins yields STX rewards, which can
-                  further be stacked to yield BTC rewards.
-                </p>
-              </div>
-
-              <p className="card-link mb-5">
-                <button className="btn btn-outline-primary" type="button" onClick={handleOpenAuth}>
-                  Get Started!
-                </button>
-              </p>
-
-              <div className="card-footer">
-                <a href="https://docs.citycoins.co" rel="noopener" target="_blank">
-                  View our documentation for more info!
-                </a>
-              </div>
-            </div>
-          </div>
+        <div className="container pt-3">
+          <h1>Introducing CityCoins</h1>
+          <p className="h5">
+            CityCoins are cryptocurrencies that allow you to support your favorite cities while
+            earning yield in Bitcoin.
+          </p>
+          <p className="h5">
+            Each city has their own coin, starting with Miami and MiamiCoin ($MIA).
+          </p>
+          <h2>Activation</h2>
+          <p className="h5">
+            CityCoins only exist through mining, which requires 20 independent wallets to signal
+            activation once the contract is deployed.
+          </p>
+          <p className="h5">No ICO, no pre-sale, no pre-mine.</p>
+          <h2>Mining</h2>
+          <p className="h5">
+            Anyone can mine CityCoins by forwarding STX into a CityCoins smart contract on the
+            Stacks blockchain.
+          </p>
+          <p className="h5">
+            30% of the STX that miners forward is sent directly to a reserved wallet for the city.
+          </p>
+          <h2>Stacking</h2>
+          <p className="h5">
+            The remaining 70% of the STX that miners forward is distributed to CityCoin holders who
+            stack their tokens.
+          </p>
+          <p className="h5">
+            Stacking CityCoins yields STX rewards, which can further be stacked to yield BTC
+            rewards.
+          </p>
+          <button className="btn btn-lg btn-outline-primary" type="button" onClick={handleOpenAuth}>
+            Get Started!
+          </button>
+          <a
+            className="btn btn-lg btn-outline-primary m-3"
+            href="https://docs.citycoins.co"
+            rel="noopener"
+            target="_blank"
+          >
+            Documentation
+          </a>
         </div>
       </div>
     </div>
