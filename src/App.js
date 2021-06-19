708323e3e9cce4819e33d493eff4199ec804ba2f
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-06-19 06:40:44 -0700

    add title text to header

--- src/App.js
+++ src/App.js
@@ -24,14 +24,18 @@ export default function App(props) {
 
   return (
     <Connect authOptions={authOptions}>
-      <header className="d-flex flex-wrap justify-content-evenly align-items-center py-3 mb-4 border-bottom">
-        <a
-          href="/"
-          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
-        >
-          <img src="/citycoin-icon-blue-reversed-75x75.png" width="75" alt="CityCoins CC Logo" />
-        </a>
-
+      <header className="d-flex flex-wrap justify-content-between align-items-center mx-3 py-3 mb-4 border-bottom">
+        <div>
+          <a
+            href="/"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+          >
+            <img src="/citycoin-icon-blue-reversed-75x75.png" width="75" alt="CityCoins CC Logo" />
+          </a>
+        </div>
+        <div>
+          <span className="h1">CityCoins</span>
+        </div>
         <div className="btn-group btn-group-lg" role="group" aria-label="Basic outlined example">
           <button type="button" className="btn btn-outline-primary">
             Home
