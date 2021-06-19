f2cd306c932960f022274ae85ab3d5da6a26987c
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-06-19 08:05:35 -0700

    add recent transactions to CityCoin page

--- src/pages/CityCoin.js
+++ src/pages/CityCoin.js
@@ -13,6 +13,8 @@ export default function CityCoin({ userSession }) {
   return (
     <main className="container">
       <CityCoinContainer />
+      <hr />
+      <CityCoinTxList />
     </main>
   );
 
