6dbb6f00b880ca693e683c6dd438033899518fbc
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-06-23 10:24:17 -0700

    remove main button and tx status at bottom

--- src/components/CityCoinStackingClaim.js
+++ src/components/CityCoinStackingClaim.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useConnect } from '@stacks/connect-react';
 import { CC_NAME, CITYCOIN_CONTRACT_NAME, CONTRACT_ADDRESS, NETWORK } from '../lib/constants';
-import { TxStatus } from './TxStatus';
 import {
   uintCV,
   PostConditionMode,
@@ -60,6 +59,8 @@ export function CityCoinStackingClaim({ ownerStxAddress }) {
     });
   };
 
+  // TODO: add txstatus back to correlate with each claim button state
+
   return (
     <>
       <h3>Claim Stacking Rewards</h3>
@@ -91,29 +92,6 @@ export function CityCoinStackingClaim({ ownerStxAddress }) {
       ) : loading ? null : (
         <div className="my-2">Nothing to claim</div>
       )}
-      <form>
-        <div className="input-group mb-3">
-          <input
-            type="number"
-            className="form-control"
-            ref={rewardCycleRef}
-            aria-label="Reward Cycle"
-            placeholder="Reward Cycle"
-            required
-            minLength="1"
-          />
-        </div>
-        <button className="btn btn-block btn-primary" type="button" onClick={claimAction}>
-          <div
-            role="status"
-            className={`${
-              loading ? '' : 'd-none'
-            } spinner-border spinner-border-sm text-info align-text-top mr-2`}
-          />
-          Claim Stacking Rewards
-        </button>
-      </form>
-      {txId && <TxStatus txId={txId} />}
     </>
   );
 }
