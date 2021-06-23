7da27bfadd9d56238e1234bb8ea1ebbd221f3c36
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-06-23 16:20:17 -0700

    fix class name on tx list icon

--- src/components/CityCoinTxList.js
+++ src/components/CityCoinTxList.js
@@ -221,7 +221,7 @@ function Details({ tx }) {
       <div className="col-lg-6 col-md-12 text-right">
         {tx.tx_id.substr(0, 10)}...
         <a href={`https://explorer.stacks.co/txid/${tx.tx_id}`} target="_blank" rel="noreferrer">
-          <i class="bi bi-box-arrow-up-right" />
+          <i className="bi bi-box-arrow-up-right" />
         </a>
       </div>
     </>
