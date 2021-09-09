65ed3f7d2e135f6e6290bc4ebda0c4ef6c335144
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-09-09 08:26:10 -0700

    chore: unused useref

--- src/components/CityCoinStackingClaim.js
+++ src/components/CityCoinStackingClaim.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useConnect } from '@stacks/connect-react';
 import {
   CONTRACT_DEPLOYER,
