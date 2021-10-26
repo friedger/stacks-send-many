5f15973af6a04d5107867f565bdb6acd42ac97a0
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-10-26 12:18:50 -0700

    feat: add nav to city selection, fix styles

--- src/components/common/SelectCity.js
+++ src/components/common/SelectCity.js
@@ -1,9 +1,24 @@
+import { navigate } from '@reach/router';
+
+const navToCity = city => {
+  if (city === 'none' || city === undefined) {
+    navigate('/');
+  } else {
+    navigate(`/${city}`);
+  }
+};
+
 export default function SelectCity() {
   // TODO: these should be stored somewhere else and iterated over to create this component
 
   return (
-    <select className="form-select" aria-label="Select a City" defaultValue={'none'}>
-      <option value="none">Select a City</option>
+    <select
+      className="form-select"
+      aria-label="Select a City"
+      defaultValue={'none'}
+      onChange={e => navToCity(e.currentTarget.value)}
+    >
+      <option value="none">Select a City...</option>
       <option value="atx">Austin, TX</option>
       <option value="mia">Miami, FL</option>
       <option value="nyc">New York, NY</option>
