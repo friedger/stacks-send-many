c068657a0fe54d64c2c5720971511537049ca26f
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-10-26 10:52:54 -0700

    fix: update select city format for react

--- src/components/common/SelectCity.js
+++ src/components/common/SelectCity.js
@@ -1,16 +1,11 @@
 export default function SelectCity() {
   return (
-    <select className="form-select" aria-label="Select a City">
-      <option selected value="0">
-        Select a City
-      </option>
-      <option value="1">Fort Lauderdale</option>
-      <option value="2">Jacksonville</option>
-      <option value="3">Miami</option>
-      <option value="4">Orlando</option>
-      <option value="5">Port St. Lucie</option>
-      <option value="6">St. Petersburg</option>
-      <option value="7">Tampa</option>
+    <select className="form-select" aria-label="Select a City" defaultValue={'none'}>
+      <option value="none">Select a City</option>
+      <option value="atx">Austin, TX</option>
+      <option value="mia">Miami, FL</option>
+      <option value="nyc">New York, NY</option>
+      <option value="sfo">San Fransciso, CA</option>
     </select>
   );
 }
