c8079376dcaf8a98d17a9edd2f33f3c497da4dd7
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-10-26 14:13:24 -0700

    fix: condense landing page content

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -22,64 +22,69 @@ export default function Landing({ userSession }) {
             <h1>Introducing CityCoins</h1>
             <p className="h5">
               CityCoins gives communities the power to improve their cities, while providing crypto
-              rewards to individual contributors and city governments alike.
-            </p>
-            <p className="h5">
-              Each city has their own coin, starting with{' '}
+              rewards to individual contributors and city governments alike. Each city has their own
+              coin, starting with{' '}
               <a
                 href="https://www.citycoins.co/miamicoin"
                 className="text-decoration-none"
                 target="_blank"
                 rel="noreferrer"
               >
-                Miami and MiamiCoin ($MIA)
+                Miami and MiamiCoin (MIA)
               </a>
               .
             </p>
-            <h2 className="mt-4">Activation</h2>
-            <p className="h5">
-              CityCoins only exist through mining, which does not begin until 20 independent wallets
-              signal activation once the contract is deployed.
-            </p>
-            <p className="h5">No ICO, no pre-sale, no pre-mine.</p>
-            <h2 className="mt-4">Mining</h2>
-            <p className="h5">
-              Anyone can mine CityCoins by forwarding STX into a CityCoins smart contract on the
-              Stacks blockchain.
-            </p>
-            <p className="h5">
-              30% of the STX that miners forward is sent directly to a reserved wallet for the city.
-            </p>
-            <h2 className="mt-4">Stacking</h2>
-            <p className="h5">
-              The remaining 70% of the STX that miners forward is distributed to CityCoin holders
-              who Stack their tokens.
-            </p>
-            <p className="h5">
-              Stacking requires holders to lock their CityCoins for determined “reward cycles”,
-              where they are transferred to the smart contract and can be reclaimed after the cycles
-              are complete.
-            </p>
-            <p className="h5">
-              Stacking CityCoins earns STX rewards from CityCoin miners. STX rewards can further be
-              stacked on Stacks to earn BTC rewards.
-            </p>
-            <a
-              href="https://docs.citycoins.co"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-lg btn-outline-primary mt-4"
-            >
-              Read the Docs
-            </a>
-            <button
-              className="btn btn-lg btn-outline-primary mt-4 ms-3"
-              type="button"
-              onClick={handleOpenAuth}
-            >
-              Get Started!
-            </button>
-            <p className="mt-3 fs-6 fst-italic">* Requires the Stacks Web Wallet from Hiro</p>
+            <div className="row">
+              <div className="col-md">
+                <h2 className="mt-4">
+                  <i className="bi bi-activity"></i> Activation
+                </h2>
+                <p className="h5">
+                  CityCoins only exist through mining, which does not begin until 20 independent
+                  wallets signal activation after the contract is deployed. No ICO, no pre-sale, no
+                  pre-mine.
+                </p>
+              </div>
+              <div className="col-md">
+                <h2 className="mt-4">Mining</h2>
+                <p className="h5">
+                  Anyone can mine CityCoins by forwarding STX into a CityCoins smart contract on the
+                  Stacks blockchain. 30% of the STX that miners forward is sent directly to a
+                  reserved wallet for the city.
+                </p>
+              </div>
+              <div className="col-md">
+                <h2 className="mt-4">Stacking</h2>
+                <p className="h5">
+                  Anyone can Stack CityCoins by locking them in a CityCoins smart contract for
+                  determined "reward cycles", and recieve a portion of the remaining 70% of the STX
+                  sent by miners.
+                </p>
+              </div>
+            </div>
+            <div className="text-center mt-3">
+              <a
+                href="https://docs.citycoins.co"
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-lg btn-outline-primary mt-4"
+              >
+                Read the Docs
+              </a>
+              <button
+                className="btn btn-lg btn-outline-primary mt-4 ms-3"
+                type="button"
+                onClick={handleOpenAuth}
+              >
+                Get Started!
+              </button>
+              <p className="mt-3 fs-6 fst-italic">
+                * Requires the{' '}
+                <a href="https://www.hiro.so/wallet/install-web" target="_blank" rel="noreferrer">
+                  Hiro Wallet Browser Extension
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
