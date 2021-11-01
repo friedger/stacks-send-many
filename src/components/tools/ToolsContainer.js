2451c5307e0300cc25fea79ae3542fb5c75b5a72
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-11-01 15:16:52 -0700

    feat: add nav pills to tools page

--- src/components/tools/ToolsContainer.js
+++ src/components/tools/ToolsContainer.js
@@ -1,8 +1,43 @@
+import NotDeployed from '../common/NotDeloyed';
+
 export default function ToolsContainer(props) {
-  return (
-    <div>
-      <h3>Tools</h3>
-      <p>An {props.token.symbol} sandbox on steroids.</p>
-    </div>
-  );
+  if (props.contracts.deployer === '') {
+    return <NotDeployed />;
+  } else {
+    return (
+      <div className="d-flex flex-column flex-md-row align-items-center align-items-md-start">
+        <div
+          className="nav flex-column nav-pills mx-auto mx-md-0 me-md-3 text-nowrap"
+          id="v-pills-tab"
+          role="tablist"
+          aria-orientation="vertical"
+        >
+          <button
+            className="nav-link active"
+            id="v-pills-tools-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#tools"
+            type="button"
+            role="tab"
+            aria-controls="tools"
+            aria-selected="true"
+          >
+            Tools
+          </button>
+        </div>
+        <div className="tab-content" id="v-pills-tabContent">
+          <hr className="d-md-none" />
+          <div
+            className="tab-pane fade show active"
+            id="tools"
+            role="tabpanel"
+            aria-labelledby="v-pills-tools-tab"
+          >
+            <h3>Tools</h3>
+            <p>An {props.token.symbol} sandbox on steroids.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
