12bb01f06f642747a7d218b66693f26ef4997459
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-11-01 08:04:51 -0700

    chore: code cleanup

--- src/pages/actions/Dashboard.js
+++ src/pages/actions/Dashboard.js
@@ -1,6 +1,3 @@
-import MiningActivity from '../../components/dashboard/MiningActivity';
-import StackingActivity from '../../components/dashboard/StackingActivity';
-import TransactionLog from '../../components/dashboard/TransactionLog';
 import NavBackHome from '../../components/common/NavBackHome';
 import NavBar from '../../components/common/NavBar';
 import DashboardContainer from '../../components/dashboard/DashboardContainer';
