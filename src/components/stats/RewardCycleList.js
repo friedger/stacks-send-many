08de2c4f671569aa237f04e2482d183c27fb81f3
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-11-02 17:06:16 -0700

    feat: add reward cycles

--- src/components/stats/RewardCycleList.js
+++ src/components/stats/RewardCycleList.js
@@ -2,14 +2,11 @@ export default function RewardCycleList(props) {
   const startBlock = props.config.startBlock;
   const cycleLength = props.config.rewardCycleLength;
   const numCycles = 7;
-
-  // 0, 24497, 26596
-  // 1, 26597, 28696
-  // 2, 28697, 30796
-  // 3, 30797, 32996
-  // 4, 32997, 35197
-  // 5, 35198, 37396
-  // 6, 37397, 39596
+  const cycleLengths = [];
+  for (let i = 0; i < numCycles; i++) {
+    cycleLengths.push(cycleLength * i);
+  }
+  console.log(cycleLengths);
 
   return (
     <div className="col-lg-6">
@@ -20,45 +17,20 @@ export default function RewardCycleList(props) {
           <div className="col-sm-4">Start Block</div>
           <div className="col-sm-4">End Block</div>
         </div>
-
-        <hr className="d-sm-none" />
-        <div className="row text-center text-sm-start">
-          <div className="col-sm-4">0</div>
-          <div className="col-sm-4">{props.config.startBlock.toLocaleString()}</div>
-          <div className="col-sm-4">{(startBlock + cycleLength - 1).toLocaleString()}</div>
-        </div>
-        <hr className="d-sm-none" />
-        <div className="row text-center text-sm-start">
-          <div className="col-sm-4">1</div>
-          <div className="col-sm-4">24,497</div>
-          <div className="col-sm-4">34,497</div>
-        </div>
-        <hr className="d-sm-none" />
-        <div className="row text-center text-sm-start">
-          <div className="col-sm-4">2</div>
-          <div className="col-sm-4">24,497</div>
-          <div className="col-sm-4">34,497</div>
-        </div>
-        <div className="row text-center text-sm-start">
-          <div className="col-sm-4">3</div>
-          <div className="col-sm-4">24,497</div>
-          <div className="col-sm-4">34,497</div>
-        </div>
-        <div className="row text-center text-sm-start">
-          <div className="col-sm-4">4</div>
-          <div className="col-sm-4">24,497</div>
-          <div className="col-sm-4">34,497</div>
-        </div>
-        <div className="row text-center text-sm-start">
-          <div className="col-sm-4">5</div>
-          <div className="col-sm-4">24,497</div>
-          <div className="col-sm-4">34,497</div>
-        </div>
-        <div className="row text-center text-sm-start">
-          <div className="col-sm-4">6</div>
-          <div className="col-sm-4">24,497</div>
-          <div className="col-sm-4">34,497</div>
-        </div>
+        {cycleLengths.map((value, idx) => {
+          return (
+            <>
+              <hr className="d-sm-none" />
+              <div className="row text-center text-sm-start">
+                <div className="col-sm-4">{idx}</div>
+                <div className="col-sm-4">{(startBlock + value).toLocaleString()}</div>
+                <div className="col-sm-4">
+                  {(startBlock + value + cycleLength - 1).toLocaleString()}
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
