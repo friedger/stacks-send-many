baec8831210604c3c196734f257224a4b8ec84b0
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-11-06 05:03:20 -0700

    fix: no divs in ps

--- src/components/common/LoadingSpinner.js
+++ src/components/common/LoadingSpinner.js
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div
+    <span
       role="status"
       className="spinner-border spinner-border-sm text-info align-text-top ms-1 me-2"
     />
