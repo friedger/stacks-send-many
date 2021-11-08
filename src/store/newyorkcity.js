ea3c56da101c9f1764e8e49ce824a8fbc9963644
Lukasz <829210+LNow@users.noreply.github.com>
2021-11-09 00:34:38 +0100

    fix: add missing ATX/NYC testnet config

--- src/store/newyorkcity.js
+++ src/store/newyorkcity.js
@@ -15,6 +15,7 @@ export const NYC_TOKEN = {
 export const NYC_CONFIG = {
   cityName: 'New York City',
   cityWallet: testnet ? 'ST24A7X58T6QS7P56J8KGBRTQ0SEWVR4VRFQMD9JT' : '',
-  rewardCycleLength: 0,
-  startBlock: 0,
+  halving: 210000,
+  rewardCycleLength: 2100,
+  startBlock: testnet ? 19302 : 0,
 };
