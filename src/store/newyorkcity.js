75f6e7062dd057b799a6423303e63fbf551db88d
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-11-09 16:17:03 -0700

    feat: add nyc deployer and wallet address for mainnet

--- src/store/newyorkcity.js
+++ src/store/newyorkcity.js
@@ -1,7 +1,9 @@
 import { testnet } from '../lib/stacks';
 
 export const NYC_CONTRACTS = {
-  deployer: testnet ? 'ST3ESBKBNA2PTSQZFMC729RAX1YBSBXDGJK8SMF1Y' : '',
+  deployer: testnet
+    ? 'ST3ESBKBNA2PTSQZFMC729RAX1YBSBXDGJK8SMF1Y'
+    : 'SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5',
   coreContract: 'newyorkcitycoin-core-v1',
   authContract: 'newyorkcitycoin-auth',
   tokenContract: 'newyorkcitycoin-token',
@@ -14,7 +16,9 @@ export const NYC_TOKEN = {
 
 export const NYC_CONFIG = {
   cityName: 'New York City',
-  cityWallet: testnet ? 'ST24A7X58T6QS7P56J8KGBRTQ0SEWVR4VRFQMD9JT' : '',
+  cityWallet: testnet
+    ? 'ST24A7X58T6QS7P56J8KGBRTQ0SEWVR4VRFQMD9JT'
+    : 'SM18VBF2QYAAHN57Q28E2HSM15F6078JZYZ2FQBCX',
   halving: 210000,
   rewardCycleLength: 2100,
   startBlock: testnet ? 19302 : 0,
