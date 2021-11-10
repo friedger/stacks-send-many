b530ea298cd63d1212810e0848834b495fc73d06
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-11-10 14:41:05 -0700

    fix: update txid link href

--- src/components/common/LinkTx.js
+++ src/components/common/LinkTx.js
@@ -4,7 +4,11 @@ export default function LinkTx({ txId, shorten = true }) {
   !txId.startsWith('0x') && (txId = `0x${txId}`);
   const url = `https://explorer.stacks.co/txid/${txId}`;
   return (
-    <a href={url + testnet ? '?chain=testnet' : '?chain=mainnet'} target="_blank" rel="noreferrer">
+    <a
+      href={`${url}${testnet ? '?chain=testnet' : '?chain=mainnet'}`}
+      target="_blank"
+      rel="noreferrer"
+    >
       {shorten ? txId.substr(0, 5) + '...' + txId.substr(txId.length - 5) : txId}
       <i className="bi bi-box-arrow-up-right ms-1" />
     </a>
