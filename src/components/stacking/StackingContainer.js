a96264b10e4e4d6eda0a73da161467ec898ba83b
Jason Schrader <whoabuddy@users.noreply.github.com>
2022-05-10 15:02:04 -0700

    fix: test removing activation/initialization requirements

--- src/components/stacking/StackingContainer.js
+++ src/components/stacking/StackingContainer.js
@@ -1,67 +1,11 @@
 import StackCityCoins from './StackCityCoins';
 import ClaimStackingRewards from './ClaimStackingRewards';
 import StackingTools from './StackingTools';
-import NotDeployed from '../common/NotDeployed';
-import { useAtom } from 'jotai';
-import {
-  currentBlockHeight,
-  currentCityActivationStatus,
-  currentCityInitialized,
-} from '../../store/common';
-import { getActivationStatus, isInitialized } from '../../lib/citycoins';
-import { useEffect } from 'react';
-import NotActivated from '../common/NotActivated';
-import ActivationCountdown from '../common/ActivationCountdown';
-import NotInitialized from '../common/NotInitialized';
 
 // TODO: stacking should display a message if contract is not activated
 // else load the stacking content
 
 export default function StackingContainer(props) {
-  const [initialized, setInitialized] = useAtom(currentCityInitialized);
-  const [cityActivated, setCityActivated] = useAtom(currentCityActivationStatus);
-  const [blockHeight] = useAtom(currentBlockHeight);
-
-  useEffect(() => {
-    isInitialized(props.contracts.deployer, props.contracts.authContract)
-      .then(result => {
-        setInitialized(result);
-      })
-      .catch(err => {
-        setInitialized(false);
-        console.log(err);
-      });
-    getActivationStatus(props.contracts.deployer, props.contracts.coreContract)
-      .then(result => {
-        setCityActivated(result.value);
-      })
-      .catch(err => {
-        setCityActivated(false);
-        console.log(err);
-      });
-  }, [props.contracts.authContract, props.contracts.coreContract, props.contracts.deployer]);
-
-  if (props.contracts.deployer === '') {
-    return <NotDeployed />;
-  }
-
-  if (!initialized) {
-    return <NotInitialized />;
-  }
-
-  if (!cityActivated) {
-    return <NotActivated symbol={props.token.symbol} />;
-  }
-
-  if (blockHeight < props.config.startBlock) {
-    return (
-      <ActivationCountdown
-        symbol={props.token.symbol}
-        blocks={props.config.startBlock - blockHeight}
-      />
-    );
-  }
-
   return (
     <div className="d-flex flex-column flex-md-row align-items-center align-items-md-start">
       <div
