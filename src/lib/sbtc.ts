45b7cd99c65ae0f7ef2645c3cc45aff1f2258810
hz <gravatar.m2a16@simplelogin.com>
2024-05-04 22:58:36 +0300

    fix sBTC public key part

--- src/lib/sbtc.ts
+++ src/lib/sbtc.ts
@@ -1,19 +1,19 @@
 import { p2tr } from '@scure/btc-signer';
-import { callReadOnlyFunction } from '@stacks/transactions';
+import { BufferCV, SomeCV, callReadOnlyFunction } from '@stacks/transactions';
 import { MAINNET, REGTEST, TESTNET } from 'sbtc';
 import { NETWORK, mainnet, testnet } from './constants';
 
 export async function getSbtcWalletAddress(assetContract: string) {
   const [contractAddress, contractName] = assetContract.split('.');
 
-  const publicKey = await callReadOnlyFunction({
+  const publicKey = (await callReadOnlyFunction({
     contractAddress,
     contractName,
     functionName: 'get-bitcoin-wallet-public-key',
     functionArgs: [],
     senderAddress: contractAddress,
     network: NETWORK,
-  });
+  })) as SomeCV<BufferCV>;
 
   let bitcoinNetwork;
   if (mainnet) {
@@ -24,7 +24,7 @@ export async function getSbtcWalletAddress(assetContract: string) {
     bitcoinNetwork = REGTEST;
   }
 
-  const addr = p2tr(publicKey, undefined, bitcoinNetwork).address;
+  const addr = p2tr(publicKey.value.buffer, undefined, bitcoinNetwork).address;
   console.log(addr);
   return addr;
 }
