e25af9921aa113cd1da10baf8bcf74ff9cdf756c
hz <gravatar.m2a16@simplelogin.com>
2024-05-04 23:00:52 +0300

    sBTC code

--- src/lib/sbtc.ts
+++ src/lib/sbtc.ts
@@ -1,12 +1,14 @@
 import { p2tr } from '@scure/btc-signer';
 import { BufferCV, SomeCV, callReadOnlyFunction } from '@stacks/transactions';
-import { MAINNET, REGTEST, TESTNET } from 'sbtc';
+import { BitcoinNetwork, MAINNET, REGTEST, TESTNET } from 'sbtc';
 import { NETWORK, mainnet, testnet } from './constants';
 
 export async function getSbtcWalletAddress(assetContract: string) {
   const [contractAddress, contractName] = assetContract.split('.');
 
-  const publicKey = (await callReadOnlyFunction({
+  const {
+    value: { buffer: publicKey },
+  } = (await callReadOnlyFunction({
     contractAddress,
     contractName,
     functionName: 'get-bitcoin-wallet-public-key',
@@ -15,16 +17,18 @@ export async function getSbtcWalletAddress(assetContract: string) {
     network: NETWORK,
   })) as SomeCV<BufferCV>;
 
-  let bitcoinNetwork;
+  let bitcoinNetwork: BitcoinNetwork = REGTEST;
   if (mainnet) {
     bitcoinNetwork = MAINNET;
   } else if (testnet) {
     bitcoinNetwork = TESTNET;
-  } else {
-    bitcoinNetwork = REGTEST;
   }
 
-  const addr = p2tr(publicKey.value.buffer, undefined, bitcoinNetwork).address;
+  const addr = p2tr(
+    publicKey.length === 33 ? publicKey.subarray(1) : publicKey, // strip y byte
+    undefined,
+    bitcoinNetwork
+  );
   console.log(addr);
   return addr;
 }
